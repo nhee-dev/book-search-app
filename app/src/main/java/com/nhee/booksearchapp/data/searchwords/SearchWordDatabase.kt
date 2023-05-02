d60729e05d36b3cce4b9345f37bb52edccd3282b
Namhee <nhee0410@naver.com>
2023-05-02 16:20:46 +0900

    fix: Room 데이터베이스를 Hilt로 의존성 주입하면서 불필요해진 코드 삭제

--- app/src/main/java/com/nhee/booksearchapp/data/searchwords/SearchWordDatabase.kt
+++ app/src/main/java/com/nhee/booksearchapp/data/searchwords/SearchWordDatabase.kt
@@ -10,22 +10,4 @@ abstract class SearchWordDatabase : RoomDatabase() {
 
     abstract fun searchWordDao(): SearchWordDao
 
-    // Hilt 쓰면서 없어진 부분
-//    companion object {
-//        @Volatile
-//        private var INSTANCE: SearchWordDatabase? = null
-//
-//        fun getDatabase(context: Context): SearchWordDatabase {
-//            return INSTANCE ?: synchronized(this) {
-//                val instance = Room.databaseBuilder(
-//                    context.applicationContext,
-//                    SearchWordDatabase::class.java,
-//                    "search_word_database"
-//                ).build()
-//                INSTANCE = instance
-//
-//                instance
-//            }
-//        }
-//    }
 }
